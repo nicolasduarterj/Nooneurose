6401f81b1e9a81d16536c5951cf9e89a8e84fecc
Victor <victorhugotoliveira@gmail.com>
2026-04-24 16:12:21 -0300

    feat: adiciona o tipo message
    - Altera a referência a mensagem no componente ChatMessage

--- frontend/components/ChatMessage.tsx
+++ frontend/components/ChatMessage.tsx
@@ -1,14 +1,12 @@
+import { Message } from "@/app/types/message";
 import { BotMessageSquare, CircleUser } from "lucide-react";
 
-type MessageType = "user" | "ai";
-
 type ChatMessageProps = {
-  type: MessageType;
-  content: string;
+  message: Message;
 };
 
-export default function ChatMessage({ type, content }: ChatMessageProps) {
-  const isUser = type === "user";
+export default function ChatMessage({ message }: ChatMessageProps) {
+  const isUser = message.type === "user";
 
   return (
     <div className={`flex ${isUser ? 'flex-row-reverse' : 'flex-row'} gap-2.5 sm:px-3 sm:py-3`}>
@@ -21,7 +19,7 @@ export default function ChatMessage({ type, content }: ChatMessageProps) {
         }
       </div>
       <div className="flex min-w-0 flex-1 rounded-3xl border-secondary bg-secondary px-4 py-4 text-neutral drop-shadow-neutral-950">
-        <p className="text-base text-justify whitespace-pre-wrap wrap-anywhere">{content}</p>
+        <p className="text-base text-justify whitespace-pre-wrap wrap-anywhere">{message.content}</p>
       </div>
     </div>
   );
