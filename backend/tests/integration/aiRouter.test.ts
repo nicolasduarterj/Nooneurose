c5790d5d6eb2b6dbc2d99b51523a51d0714c1ef1
Nicolas Duarte <nicolas_duarterj@proton.me>
2026-04-24 19:40:50 -0300

    test: add test and fix .env examples

--- backend/tests/integration/aiRouter.test.ts
+++ backend/tests/integration/aiRouter.test.ts
@@ -16,7 +16,15 @@ describe('AI Router', () => {
         test('Missing message returns 400', async () => {
             const response = await request(app)
                 .post(`${Paths._}${Paths.AI._}${Paths.AI.Send}`)
-                .send({ message: '' })
+                .send({ message: '', chatUUID: '4f8439ab-20a8-4ea5-9d17-8991dc0f136d' })
+
+            expect(response.status).toBe(400)
+        })
+
+        test('Missing chat uuid return 400', async () => {
+            const response = await request(app)
+                .post(`${Paths._}${Paths.AI._}${Paths.AI.Send}`)
+                .send({ message: 'Lorem ipsum', chatUUID: '' })
 
             expect(response.status).toBe(400)
         })
