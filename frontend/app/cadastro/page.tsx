c84ec39862ffcee85d10caa7286357c3159e2a5d
ZackViros51 <davidsomagalhaes@hotmail.com>
2026-05-11 20:27:18 -0300

    Criação da tela de login

---
+++ frontend/app/cadastro/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Cadastro() {
+
+    return (
+        <main>
+            <div>
+                
+            </div>
+        </main>
+
+    );
+}
