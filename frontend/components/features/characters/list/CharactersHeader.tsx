ec29f7307f34213addb9fb1bf9a0725d695ec288
Victor <victorhugotoliveira@gmail.com>
2026-05-14 19:58:25 -0300

    feat: adiciona componente de header da listagem de personagem

---
+++ frontend/components/features/characters/list/CharactersHeader.tsx
@@ -0,0 +1,23 @@
+import { Plus } from "lucide-react";
+
+export function CharactersHeader() {
+    return (
+        <div className="flex items-center justify-between px-2">
+            <div>
+                <h1 className="text-2xl font-bold">Personagens disponíveis</h1>
+            </div>
+            <div className="flex gap-2">
+                <button
+                    type="button"
+                    className="bg-primary/50 px-8 py-1 rounded-lg text-neutral/80 hover:bg-primary/70 cursor-pointer">
+                    Meus personagens
+                </button>
+                <button
+                    type="button"
+                    className="bg-primary/50 px-1 py-1 rounded-lg text-neutral/80 hover:bg-primary/70 cursor-pointer">
+                    <Plus />
+                </button>
+            </div>
+        </div>
+    )
+}
