a002f8275ffda3c6db2cd477f2c429adcc4092b1
Victor <victorhugotoliveira@gmail.com>
2026-05-18 20:42:50 -0300

    fix: corrige renderização

--- frontend/app/(with-sidebar)/characters/[id]/page.tsx
+++ frontend/app/(with-sidebar)/characters/[id]/page.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { Separator } from "@/components/ui/separator";
 import CharacterView from "@/components/features/characters/view/CharacterView";
 import { Character } from "@/types/character";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? "";
 
@@ -13,14 +16,29 @@ type CharacterPageProps = {
     params: Promise<CharacterPageParams>;
 }
 
-export default async function CharacterPage({ params }: CharacterPageProps) {
-    const { id } = await params;
-    const res = await fetch(`${API_BASE}/api/character/byId/${id}`);
+export default function CharacterPage({ params }: CharacterPageProps) {
+    const [character, setCharacter] = useState<Character>();
+    const router = useRouter();
+    const { id } = use<CharacterPageParams>(params);
 
-    if (!res.ok)
-        redirect("/characters");
+    useEffect(() => {
+        const fetchCharacter = async () => {
+            try {
+                const res = await fetch(`${API_BASE}/api/character/byId/${id}`);
+                if (!res.ok)
+                    router.push("/characters");
 
-    const character: Character = await res.json();
+                const characterData: Character = await res.json();
+                setCharacter(characterData);
+            }
+            catch (error) {
+                console.error("Erro ao buscar personagem:", error);
+                router.push("/characters");
+            }
+        }
+
+        fetchCharacter();
+    }, [id]);
 
     return (
         <main className="flex h-full w-full flex-col gap-4 p-6 min-h-0 relative">
@@ -31,7 +49,7 @@ export default async function CharacterPage({ params }: CharacterPageProps) {
             <Separator className="bg-primary/50" />
 
             <div className="w-full h-fit">
-                <CharacterView character={character} />
+                {character && <CharacterView character={character} />}
             </div>
         </main>
     );
