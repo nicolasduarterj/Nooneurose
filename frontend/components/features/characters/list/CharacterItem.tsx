635c4a4b85c0139b29839415236f9da2cf2ca7fc
Victor <victorhugotoliveira@gmail.com>
2026-05-18 00:30:46 -0300

    feat: adiciona navegação para visualização de personagem

--- frontend/components/features/characters/list/CharacterItem.tsx
+++ frontend/components/features/characters/list/CharacterItem.tsx
@@ -1,16 +1,19 @@
 import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
 import { Character } from "@/types/character";
 import { MessageSquareText } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 type CharacterItemProps = {
     character: Character
 }
 
 export default function CharacterItem({ character }: CharacterItemProps) {
+    const router = useRouter();
+
     return (
         <Item
             className="flex items-center justify-between rounded-md p-2 hover:bg-neutral/5">
-            <ItemContent>
+            <ItemContent onClick={() => router.push(`/characters/${character.id}`)}>
                 <ItemTitle>{character.name}</ItemTitle>
                 <ItemDescription>{character.description}</ItemDescription>
             </ItemContent>
