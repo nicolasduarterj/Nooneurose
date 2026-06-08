8aba914e7d4fcfdccbc096d0c311be06fe372982
Victor <victorhugotoliveira@gmail.com>
2026-06-07 22:27:33 -0300

    feat: inclui nome do dono na visualização de personagem

--- frontend/components/features/characters/view/CharacterView.tsx
+++ frontend/components/features/characters/view/CharacterView.tsx
@@ -8,6 +8,9 @@ import { Pencil } from "lucide-react";
 import CharacterDeleteDialog from "../delete/CharacterDeleteDialog";
 import CharacterDeriveButton from "../derive/CharacterDeriveButton";
 import { isImageUrl } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { API_BASE, authHeaders } from "@/lib/api";
+import { User } from "@/types/user";
 
 type CharacterViewProps = {
     character: Character;
@@ -16,9 +19,42 @@ type CharacterViewProps = {
 export default function CharacterView({ character }: CharacterViewProps) {
     const { user } = useAuth();
     const router = useRouter();
+    const [ownerName, setOwnerName] = useState<string>("Usuário");
     const isOwner = user?.id === character.ownerId;
     const url = character.imageURL;
     const srcDaImagem = url && isImageUrl(url) ? url : "/question.svg";
+
+    const redirectToCreator = () => {
+        if (character.ownerId === user?.id) {
+            router.push("/user/me");
+            return;
+        }
+
+        router.push(`/user/creator/${character.ownerId}`);
+    }
+    
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const res = await fetch(`${API_BASE}/api/user/byId/${character.ownerId}`, {
+                method: "GET",
+                headers: authHeaders(),
+                });
+
+                if (!res.ok) {
+                throw new Error(`Erro ao buscar usuário: ${res.status}`);
+                }
+
+                const data: User = await res.json();
+                
+                setOwnerName(data.name);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+    loadUser();
+    }, [character, router]);
 
     return (
         <section className="flex flex-col w-full h-full gap-4 rounded-md border border-neutral/20 bg-secondary p-4 text-neutral/80 sm:p-6">
@@ -67,8 +103,8 @@ export default function CharacterView({ character }: CharacterViewProps) {
                     <dt className="text-neutral/60">Criado por</dt>
                     <dd 
                         className="w-fit font-medium cursor-pointer"
-                        onClick={() => router.push(`/user/creator/${character.ownerId}`)}>
-                            {character.ownerId}
+                        onClick={redirectToCreator}>
+                            {`${ownerName} #${character.ownerId}`}
                     </dd>
                 </div>
 
